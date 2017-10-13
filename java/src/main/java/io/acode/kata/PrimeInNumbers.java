0078b5b55c36940d87d7514f9d81b27fbaa7758a
Aaron Jeskie <ajeskie@provenir.com>
2017-10-13 13:57:20 -0400

    adding a broken kata

--- java/src/main/java/io/acode/kata/PrimeInNumbers.java
+++ java/src/main/java/io/acode/kata/PrimeInNumbers.java
@@ -2,6 +2,7 @@ package io.acode.kata;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 /**
@@ -15,8 +16,12 @@ import java.util.stream.IntStream;
 public class PrimeInNumbers {
     static List<Integer> primes = new ArrayList<>();
     static void nextPrime() {
-        primes.add(IntStream.range(primes.get(primes.size()-1), 10000)
-            .filter(n -> n % 2 != 0 || n == 2)
+        if (primes.isEmpty()) {
+            primes.add(2);
+            return;
+        }
+        primes.add(IntStream.range(primes.get(primes.size()-1) + 1, 10000)
+            .filter(n -> n % 2 != 0 || n <= 2)
             .filter(n -> IntStream.range(3, n/2)
                 .filter(i -> i % n == 0)
                 .count() == 0)
@@ -24,13 +29,66 @@ public class PrimeInNumbers {
             .getAsInt());
     }
 
+    static boolean isPrime(int i) {
+        return i >= 2 && IntStream.rangeClosed(2, (int) Math.sqrt(i))
+                            .noneMatch(n -> (i % n == 0));
+    }
+
+    static String factor(Integer current, Integer count) {
+        if (count == 1) {
+            return "(" + current + ")";
+        } else {
+            return "(" + current + "**" + count + ")";
+        }
+    }
+
+    static String factorString(List<Integer> divs) {
+        Integer current = 0;
+        int count = 0;
+        String res = "";
+        for (Integer div : divs) {
+            if (!current.equals(div)) {
+                if (!current.equals(0)) {
+                    res += factor(current, count);
+                    count = 0;
+                }
+                current = div;
+                count++;
+            } else {
+                count++;
+            }
+        }
+        return res + factor(current, count);
+    }
+
     public static String factors(int n) {
         List<Integer> divs = new ArrayList<>();
-        boolean working = true;
-        while (working) {
 
+        while (true) {
+            Boolean divided = false;
+            for (int prime: primes) {
+                if (n % prime == 0 && !divided) {
+                    divs.add(prime);
+                    n /= prime;
+                    divided = true;
+                }
+            }
+
+            while (!divided) {
+                nextPrime();
+                int next = primes.get(primes.size() - 1);
+                if (n % next == 0) {
+                    divs.add(next);
+                    n /= next;
+                    divided = true;
+                }
+            }
+
+            if (primes.contains(n)) {
+                divs.add(n);
+                break;
+            }
         }
-        return "";
-
+        return factorString(divs);
     }
 }
--- java/src/test/java/io/acode/kata/PrimeInNumbersTest.java
+++ java/src/test/java/io/acode/kata/PrimeInNumbersTest.java
@@ -1,4 +1,61 @@
 package io.acode.kata;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
 public class PrimeInNumbersTest {
+
+    @Test
+    public void getNextPrimeTest() {
+        assertThat(PrimeInNumbers.primes.isEmpty()).isTrue();
+        PrimeInNumbers.nextPrime();
+        assertThat(PrimeInNumbers.primes.isEmpty()).isFalse();
+        assertThat(PrimeInNumbers.primes.size()).isEqualTo(1);
+        PrimeInNumbers.nextPrime();
+        assertThat(PrimeInNumbers.primes.size()).isEqualTo(2);
+        PrimeInNumbers.nextPrime();
+        assertThat(PrimeInNumbers.primes.get(2)).isEqualTo(5);
+        PrimeInNumbers.nextPrime();
+        assertThat(PrimeInNumbers.primes.get(3)).isEqualTo(7);
+    }
+
+    @Test
+    public void factorTest() {
+        assertThat(PrimeInNumbers.factor(2, 4)).isEqualTo("(2**4)");
+        assertThat(PrimeInNumbers.factor(2, 1)).isEqualTo("(2)");
+        assertThat(PrimeInNumbers.factor(2, 2)).isEqualTo("(2**2)");
+        assertThat(PrimeInNumbers.factor(4, 3)).isEqualTo("(4**3)");
+
+    }
+
+    @Test
+    public void factorStringTest() {
+        Integer[] ints = {3, 2, 2, 5, 2, 5, 5};
+        List<Integer> integers = new ArrayList<>();
+        integers.addAll(Arrays.asList(ints));
+
+        String factors = PrimeInNumbers.factorString(integers);
+        System.out.println(factors);
+        assertThat(factors).isEqualTo("(3)(2**2)(5)(2)(5**2)");
+    }
+
+    @Test
+    public void isPrime() {
+        assert(PrimeInNumbers.isPrime(2));
+        assert(PrimeInNumbers.isPrime(3));
+        assert(PrimeInNumbers.isPrime(5));
+        assert(PrimeInNumbers.isPrime(7));
+        assert(PrimeInNumbers.isPrime(17));
+        assert(PrimeInNumbers.isPrime(37));
+    }
+
+    @Test
+    public void factorsTest() {
+        System.out.println(PrimeInNumbers.factors(77));
+    }
 }
