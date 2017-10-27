05ab2bf80a48d385cddc9cc2a283afb03787af05
Aaron Jeskie <ajeskie@provenir.com>
2017-10-27 09:50:30 -0400

    prime factor decomp

--- java/src/main/java/io/acode/kata/PrimeInNumbers.java
+++ java/src/main/java/io/acode/kata/PrimeInNumbers.java
@@ -2,39 +2,16 @@ package io.acode.kata;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.stream.IntStream;
 
-/**
- * Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
-
- "(p1**n1)(p2**n2)...(pk**nk)"
- with the p(i) in increasing order and n(i) empty if n(i) is 1.
-
- Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
- */
 public class PrimeInNumbers {
-    static List<Integer> primes = new ArrayList<>();
-    static void nextPrime() {
-        if (primes.isEmpty()) {
-            primes.add(2);
-            return;
-        }
-        primes.add(IntStream.range(primes.get(primes.size()-1) + 1, 10000)
-            .filter(n -> n % 2 != 0 || n <= 2)
-            .filter(n -> IntStream.range(3, n/2)
-                .filter(i -> i % n == 0)
-                .count() == 0)
-            .findFirst()
-            .getAsInt());
-    }
 
     static boolean isPrime(int i) {
         return i >= 2 && IntStream.rangeClosed(2, (int) Math.sqrt(i))
-                            .noneMatch(n -> (i % n == 0));
+                .noneMatch(n -> (i % n == 0));
     }
 
-    static String factor(Integer current, Integer count) {
+    static String factorString(Integer current, Integer count) {
         if (count == 1) {
             return "(" + current + ")";
         } else {
@@ -42,14 +19,14 @@ public class PrimeInNumbers {
         }
     }
 
-    static String factorString(List<Integer> divs) {
+    static String factorDecompString(List<Integer> divs) {
         Integer current = 0;
         int count = 0;
         String res = "";
         for (Integer div : divs) {
             if (!current.equals(div)) {
                 if (!current.equals(0)) {
-                    res += factor(current, count);
+                    res += factorString(current, count);
                     count = 0;
                 }
                 current = div;
@@ -58,37 +35,24 @@ public class PrimeInNumbers {
                 count++;
             }
         }
-        return res + factor(current, count);
+        return res + factorString(current, count);
     }
 
     public static String factors(int n) {
         List<Integer> divs = new ArrayList<>();
+        int currentValue = n;
 
-        while (true) {
-            Boolean divided = false;
-            for (int prime: primes) {
-                if (n % prime == 0 && !divided) {
-                    divs.add(prime);
-                    n /= prime;
-                    divided = true;
+        while (!isPrime(currentValue)) {
+            for (int i = 1; i < currentValue / 2; i++) {
+                if (currentValue % i == 0 && isPrime(i)) {
+                    divs.add(i);
+                    currentValue /= i;
+                    break;
                 }
-            }
-
-            while (!divided) {
-                nextPrime();
-                int next = primes.get(primes.size() - 1);
-                if (n % next == 0) {
-                    divs.add(next);
-                    n /= next;
-                    divided = true;
-                }
-            }
-
-            if (primes.contains(n)) {
-                divs.add(n);
-                break;
             }
         }
-        return factorString(divs);
+        divs.add(currentValue);
+
+        return factorDecompString(divs);
     }
 }
--- java/src/test/java/io/acode/kata/PrimeInNumbersTest.java
+++ java/src/test/java/io/acode/kata/PrimeInNumbersTest.java
@@ -11,35 +11,21 @@ import static com.google.common.truth.Truth.assertThat;
 public class PrimeInNumbersTest {
 
     @Test
-    public void getNextPrimeTest() {
-        assertThat(PrimeInNumbers.primes.isEmpty()).isTrue();
-        PrimeInNumbers.nextPrime();
-        assertThat(PrimeInNumbers.primes.isEmpty()).isFalse();
-        assertThat(PrimeInNumbers.primes.size()).isEqualTo(1);
-        PrimeInNumbers.nextPrime();
-        assertThat(PrimeInNumbers.primes.size()).isEqualTo(2);
-        PrimeInNumbers.nextPrime();
-        assertThat(PrimeInNumbers.primes.get(2)).isEqualTo(5);
-        PrimeInNumbers.nextPrime();
-        assertThat(PrimeInNumbers.primes.get(3)).isEqualTo(7);
-    }
-
-    @Test
-    public void factorTest() {
-        assertThat(PrimeInNumbers.factor(2, 4)).isEqualTo("(2**4)");
-        assertThat(PrimeInNumbers.factor(2, 1)).isEqualTo("(2)");
-        assertThat(PrimeInNumbers.factor(2, 2)).isEqualTo("(2**2)");
-        assertThat(PrimeInNumbers.factor(4, 3)).isEqualTo("(4**3)");
-
-    }
-
-    @Test
     public void factorStringTest() {
+        assertThat(PrimeInNumbers.factorString(2, 4)).isEqualTo("(2**4)");
+        assertThat(PrimeInNumbers.factorString(2, 1)).isEqualTo("(2)");
+        assertThat(PrimeInNumbers.factorString(2, 2)).isEqualTo("(2**2)");
+        assertThat(PrimeInNumbers.factorString(4, 3)).isEqualTo("(4**3)");
+
+    }
+
+    @Test
+    public void decompStringTest() {
         Integer[] ints = {3, 2, 2, 5, 2, 5, 5};
         List<Integer> integers = new ArrayList<>();
         integers.addAll(Arrays.asList(ints));
 
-        String factors = PrimeInNumbers.factorString(integers);
+        String factors = PrimeInNumbers.factorDecompString(integers);
         System.out.println(factors);
         assertThat(factors).isEqualTo("(3)(2**2)(5)(2)(5**2)");
     }
@@ -56,6 +42,11 @@ public class PrimeInNumbersTest {
 
     @Test
     public void factorsTest() {
-        System.out.println(PrimeInNumbers.factors(77));
+        assertThat(PrimeInNumbers.factors(77)).isEqualTo("(7)(11)");
+        assertThat(PrimeInNumbers.factors(7775460)).isEqualTo("(2**2)(3**3)(5)(7)(11**2)(17)");
+
+        assertThat(PrimeInNumbers.factors(933555431)).isEqualTo("(7537)(123863)");
+        assertThat(PrimeInNumbers.factors(7919)).isEqualTo("(7919)");
+        assertThat(PrimeInNumbers.factors(18195729)).isEqualTo("(3)(17**2)(31)(677)");
     }
 }
