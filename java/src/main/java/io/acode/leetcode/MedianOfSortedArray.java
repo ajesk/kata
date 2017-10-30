4fea1a6e9f7ee3d65d6b09bcc8c84a24b07e7fe8
Aaron Jeskie <ajeskie@provenir.com>
2017-10-30 19:12:33 -0400

    making things work

--- java/src/main/java/io/acode/leetcode/MedianOfSortedArray.java
+++ java/src/main/java/io/acode/leetcode/MedianOfSortedArray.java
@@ -1,11 +1,19 @@
 package io.acode.leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MedianOfSortedArray {
+    private double mergeSolution(int[] nums1, int[] nums2) {
+        int[] mergeArray = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, mergeArray, 0, nums1.length);
+        System.arraycopy(nums2, 0, mergeArray, nums1.length, nums2.length);
+        int[] mergedSort = Arrays.stream(mergeArray).sorted().toArray();
+        return mergedSort.length % 2 == 0 ?
+                (mergedSort[mergedSort.length / 2] + mergedSort[mergedSort.length / 2 - 1]) / 2 :
+                mergedSort[(int)(mergedSort.length / 2 + .5)];
+    }
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        Arrays.stream(nums1).
+        return mergeSolution(nums1, nums2);
     }
 }
---
+++ java/src/test/java/io/acode/leetcode/MedianOfSortedArrayTest.java
@@ -0,0 +1,20 @@
+package io.acode.leetcode;
+
+import org.junit.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class MedianOfSortedArrayTest {
+    MedianOfSortedArray mosa = new MedianOfSortedArray();
+
+    int[][] = new int[][]
+
+    @Test
+    public void testMergeWithEven() {
+        System.out.println(mosa.findMedianSortedArrays());
+    }
+
+    @Test
+    public void testMergeWithOdd() {
+        assert false;
+    }
+}
