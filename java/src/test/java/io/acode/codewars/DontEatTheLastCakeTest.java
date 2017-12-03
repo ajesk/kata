7015e7d87cbc09194b86d46acbf61c27935613f5
Aaron Jeskie <aaronjeskie@gmail.com>
2017-12-03 08:19:26 -0500

    dont eat the last cake seed

---
+++ java/src/test/java/io/acode/codewars/DontEatTheLastCakeTest.java
@@ -0,0 +1,14 @@
+package io.acode.codewars;
+
+import static com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class DontEatTheLastCakeTest {
+    DontEatTheLastCake detlc =  new DontEatTheLastCake(0);
+
+    @Test
+    public void testSimpleFirstMove() {
+        detlc = new DontEatTheLastCake(0);
+
+    }
+}
