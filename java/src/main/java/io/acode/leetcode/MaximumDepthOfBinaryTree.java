ac4d5336964be032f0ffceed52248f8e8d5c866c
Aaron Jeskie <aaronjeskie@gmail.com>
2022-04-10 13:25:03 -0400

    more trees

---
+++ java/src/main/java/io/acode/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+package io.acode.leetcode;
+
+import io.acode.leetcode.datastructures.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+  public int maxDepth(TreeNode root) {
+    if (root == null) return 0;
+
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+}
---
+++ java/src/test/java/io/acode/leetcode/MaximumDepthOfBinaryTreeTest.java
@@ -0,0 +1,30 @@
+package io.acode.leetcode;
+
+import io.acode.leetcode.datastructures.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaximumDepthOfBinaryTreeTest {
+  MaximumDepthOfBinaryTree target = new MaximumDepthOfBinaryTree();
+
+  @Test
+  public void basicTest() {
+    assertEquals(0, target.maxDepth(null));
+  }
+
+  @Test
+  public void testSingularValue() {
+    assertEquals(1, target.maxDepth(new TreeNode(1)));
+  }
+
+  @Test
+  public void test2Layers() {
+    assertEquals(2, target.maxDepth(new TreeNode(1, new TreeNode(2), null)));
+  }
+
+  @Test
+  public void testCompetingBranches() {
+    assertEquals(3, target.maxDepth(new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)))));
+  }
+}
