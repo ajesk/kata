b22edd0dbc156d1fec58c6f37f48885e02df3fee
Aaron Jeskie <aaronjeskie@gmail.com>
2022-04-21 17:01:34 -0400

    ipad music is terrible

--- java/src/main/java/io/acode/leetcode/binarysearch/BinarySearch.java
+++ java/src/main/java/io/acode/leetcode/binarysearch/BinarySearch.java
@@ -1,4 +1,67 @@
 package io.acode.leetcode.binarysearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+  public int search(int[] nums, int target) {
+    if (nums.length == 0) return -1;
+    int pivot = nums.length / 2;
+    int pivotVal = nums[pivot];
+
+    if (nums.length == 1 && nums[0] != target) {
+      return -1;
+    }
+
+    if (pivotVal == target) {
+      return pivot;
+    }
+
+    int recurResult;
+    if (pivotVal > target) {
+      recurResult = search(Arrays.copyOfRange(nums, 0, pivot), target);
+    } else {
+      recurResult = search(Arrays.copyOfRange(nums, pivot + 1, nums.length), target);
+      if (recurResult == -1) return recurResult;
+      recurResult += pivot + 1;
+    }
+
+    return recurResult;
+  }
+
+  public int recursiveSearch(int[] nums, int target) {
+    if (nums.length == 0) return -1;
+    return recursiveSearch(nums, target, 0, nums.length - 1);
+  }
+
+  public int recursiveSearch(int[] nums, int target, int left, int right) {
+    if (right >= left) {
+      int mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (target < nums[mid]) {
+        return recursiveSearch(nums, target, left, mid - 1);
+      } else {
+        return recursiveSearch(nums, target, mid + 1, right);
+      }
+    }
+    return -1;
+  }
+
+  public int iterativeSearch(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (right >= left) {
+      int mid = (left + right) / 2;
+      if (target == nums[mid]) {
+        return mid;
+      } else if (target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return -1;
+  }
 }
--- java/src/test/java/io/acode/leetcode/binarysearch/BinarySearchTest.java
+++ java/src/test/java/io/acode/leetcode/binarysearch/BinarySearchTest.java
@@ -1,4 +1,60 @@
 package io.acode.leetcode.binarysearch;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BinarySearchTest {
+  BinarySearch target = new BinarySearch();
+
+  @Test
+  public void testBasic() {
+    assertEquals(-1, target.search(new int[]{1,2,3}, 4));
+  }
+
+  @Test
+  public void simpleSearch() {
+    assertEquals(0, target.search(new int[]{1,2,3}, 1));
+    assertEquals(1, target.search(new int[]{1,2,3}, 2));
+    assertEquals(2, target.search(new int[]{1,2,3}, 3));
+  }
+
+  @Test
+  public void testLargerValueSet() {
+    assertEquals(3, target.search(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 4));
+  }
+
+  @Test
+  public void testIterativeBasic() {
+    assertEquals(-1, target.iterativeSearch(new int[]{1,2,3}, 4));
+  }
+
+  @Test
+  public void simpleIterativeSearch() {
+    assertEquals(0, target.iterativeSearch(new int[]{1,2,3}, 1));
+    assertEquals(1, target.iterativeSearch(new int[]{1,2,3}, 2));
+    assertEquals(2, target.iterativeSearch(new int[]{1,2,3}, 3));
+  }
+
+  @Test
+  public void testIterativeLargerValueSet() {
+    assertEquals(3, target.iterativeSearch(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 4));
+  }
+
+  @Test
+  public void testRecursiveBasic() {
+    assertEquals(-1, target.recursiveSearch(new int[]{1,2,3}, 4));
+  }
+
+  @Test
+  public void simpleRecursiveSearch() {
+    assertEquals(0, target.recursiveSearch(new int[]{1,2,3}, 1));
+    assertEquals(1, target.recursiveSearch(new int[]{1,2,3}, 2));
+    assertEquals(2, target.recursiveSearch(new int[]{1,2,3}, 3));
+  }
+
+  @Test
+  public void testRecursiveLargerValueSet() {
+    assertEquals(3, target.recursiveSearch(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, 4));
+  }
 }
