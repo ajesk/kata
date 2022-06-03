c0e171ed71321b95625af43a68ed926141499057
Aaron Jeskie <aaronjeskie@gmail.com>
2022-06-02 21:19:56 -0400

    chore: bam

--- java/src/main/java/io/acode/leetcode/dp/DeleteAndEarn.java
+++ java/src/main/java/io/acode/leetcode/dp/DeleteAndEarn.java
@@ -1,4 +1,35 @@
 package io.acode.leetcode.dp;
 
+import java.util.HashMap;
+
 public class DeleteAndEarn {
+
+  HashMap<Integer, Integer> values;
+  HashMap<Integer, Integer> cache;
+
+  private int dp(int i) {
+    if (i == 0) {
+      return 0;
+    } else if (i == 1) {
+      return values.getOrDefault(1, 0);
+    }
+
+    if (cache.containsKey(i)) return cache.get(i);
+
+    int gain = values.getOrDefault(i, 0);
+    cache.put(i, Math.max(dp(i - 1), dp(i - 2) + gain));
+    return cache.get(i);
+  }
+
+  public int deleteAndEarn(int[] nums) {
+    values = new HashMap<>();
+    cache = new HashMap<>();
+    int max = 0;
+    for (int number : nums) {
+      max = Math.max(max, number);
+      values.put(number, values.getOrDefault(number, 0) + number);
+    }
+
+    return dp(max);
+  }
 }
--- java/src/test/java/io/acode/leetcode/dp/DeleteAndEarnTest.java
+++ java/src/test/java/io/acode/leetcode/dp/DeleteAndEarnTest.java
@@ -1,4 +1,32 @@
 package io.acode.leetcode.dp;
 
-public class DeleteAndEarnTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeleteAndEarnTest {
+  DeleteAndEarn target = new DeleteAndEarn();
+
+  @Test
+  void testBasic() {
+    assertEquals(1, target.deleteAndEarn(new int[]{1}));
+    assertEquals(2, target.deleteAndEarn(new int[]{1, 1}));
+  }
+
+  @Test
+  void testTwoAdjacentNumbers() {
+    assertEquals(2, target.deleteAndEarn(new int[]{2, 1}));
+    assertEquals(2, target.deleteAndEarn(new int[]{1, 2}));
+  }
+
+  @Test
+  void test3Values() {
+    assertEquals(6, target.deleteAndEarn(new int[]{3, 4, 2}));
+    assertEquals(9, target.deleteAndEarn(new int[]{2, 2, 3, 3, 3, 4}));
+  }
+
+  @Test
+  void testLeetFail() {
+    assertEquals(53, target.deleteAndEarn(new int[]{4,10,10,8,1,4,10,9,7,6}));
+  }
 }
